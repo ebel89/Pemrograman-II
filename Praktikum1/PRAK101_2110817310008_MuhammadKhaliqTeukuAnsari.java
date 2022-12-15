b2e6b372804905e1f43104dc7cd3435ac4a2d0da
Muhammad Khaliq Teuku Ansari <2110817310008@mhs.ulm.ac.id>
2022-12-15 15:12:14 +0800

    update

--- Praktikum1/PRAK101_2110817310008_MuhammadKhaliqTeukuAnsari.java
+++ Praktikum1/PRAK101_2110817310008_MuhammadKhaliqTeukuAnsari.java
@@ -11,49 +11,61 @@ import java.util.Scanner;
  * @author LENOVO
  */
 public class PRAK101_2110817310008_MuhammadKhaliqTeukuAnsari {
-     public static void main(String[]args){
-        String nama, tempatLahir, namaBulan=null;
-        int tanggalLahir, bulanLahir, tahunLahir, tinggiBadan;
-        float beratBadan;
-        Scanner keyboard = new Scanner(System.in);
-        
-        System.out.print("Masukkan Nama Lengkapku: ");
-        nama = keyboard.nextLine();
+   public static void main(String[] args) {
+      String nama, tempatLahir, namaBulan = null;
+      int tanggalLahir, bulanLahir, tahunLahir, tinggiBadan;
+      float beratBadan;
+      Scanner keyboard = new Scanner(System.in);
 
-        
-        System.out.print("Masukkan Tempat Lahir: ");
-        tempatLahir = keyboard.next();
-        
-        System.out.print("Masukkan Tanggal Lahir: ");
-        tanggalLahir = keyboard.nextInt();
-        
-        System.out.print("Masukkan Bulan Lahir: ");
-        bulanLahir = keyboard.nextInt();
-        
-        if(bulanLahir==1) namaBulan="Januari";
-        else if(bulanLahir==2) namaBulan="Februari";
-        else if(bulanLahir==3) namaBulan="Maret";
-        else if(bulanLahir==4) namaBulan="April";
-        else if(bulanLahir==5) namaBulan="Mei";
-        else if(bulanLahir==6) namaBulan="Juni";
-        else if(bulanLahir==7) namaBulan="Juli";
-        else if(bulanLahir==8) namaBulan="Agustus";
-        else if(bulanLahir==9) namaBulan="September";
-        else if(bulanLahir==10) namaBulan="Oktober";
-        else if(bulanLahir==11) namaBulan="November";
-        else if(bulanLahir==12) namaBulan="Desember";
-        
-        System.out.print("Masukkan Tahun Lahir: ");
-        tahunLahir = keyboard.nextInt();
-        
-        System.out.print("Masukkan Tinggi Badan: ");
-        tinggiBadan = keyboard.nextInt();
-        
-        System.out.print("Masukkan Berat Badan: ");
-        beratBadan = keyboard.nextFloat();
-        
-        System.out.println("Data Telah Ditambahkan");
-        System.out.format("Nama Lengkap %s ,Lahir di %s Pada Tanggal %d %s %d\n",nama,tempatLahir, tanggalLahir, namaBulan, tahunLahir);
-        System.out.format("Tinggi Badan %d cm dan Berat Badan %.2f kilogram\n",tinggiBadan ,beratBadan);
-     }
+      System.out.print("Masukkan Nama Lengkapku: ");
+      nama = keyboard.nextLine();
+
+      System.out.print("Masukkan Tempat Lahir: ");
+      tempatLahir = keyboard.next();
+
+      System.out.print("Masukkan Tanggal Lahir: ");
+      tanggalLahir = keyboard.nextInt();
+
+      System.out.print("Masukkan Bulan Lahir: ");
+      bulanLahir = keyboard.nextInt();
+
+      if (bulanLahir == 1)
+         namaBulan = "Januari";
+      else if (bulanLahir == 2)
+         namaBulan = "Februari";
+      else if (bulanLahir == 3)
+         namaBulan = "Maret";
+      else if (bulanLahir == 4)
+         namaBulan = "April";
+      else if (bulanLahir == 5)
+         namaBulan = "Mei";
+      else if (bulanLahir == 6)
+         namaBulan = "Juni";
+      else if (bulanLahir == 7)
+         namaBulan = "Juli";
+      else if (bulanLahir == 8)
+         namaBulan = "Agustus";
+      else if (bulanLahir == 9)
+         namaBulan = "September";
+      else if (bulanLahir == 10)
+         namaBulan = "Oktober";
+      else if (bulanLahir == 11)
+         namaBulan = "November";
+      else if (bulanLahir == 12)
+         namaBulan = "Desember";
+
+      System.out.print("Masukkan Tahun Lahir: ");
+      tahunLahir = keyboard.nextInt();
+
+      System.out.print("Masukkan Tinggi Badan: ");
+      tinggiBadan = keyboard.nextInt();
+
+      System.out.print("Masukkan Berat Badan: ");
+      beratBadan = keyboard.nextFloat();
+
+      System.out.println("Data Telah Ditambahkan");
+      System.out.format("Nama Lengkap %s ,Lahir di %s Pada Tanggal %d %s %d\n", nama, tempatLahir, tanggalLahir,
+            namaBulan, tahunLahir);
+      System.out.format("Tinggi Badan %d cm dan Berat Badan %.2f kilogram\n", tinggiBadan, beratBadan);
+   }
 }
